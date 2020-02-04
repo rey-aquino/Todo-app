a3e869c0d6c974ea4a997e1a8bae210129a7913b
Rey V. Aquino <49036511+includeiostreamsharp@users.noreply.github.com>
2020-02-04 09:54:13 +0800

    Count Todos and Hide Complete Tasks
    Storing temporary UI state in a Reactive Dictionary

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,16 +1,29 @@
 import { Template } from 'meteor/templating';
-
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
  
 import './body.html';
  
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
+
     // Show newest tasks at the top
     return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
   },
 });
 
@@ -31,5 +44,8 @@ Template.body.events({
  
     // Clear form
     target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
